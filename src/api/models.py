6bb86fca1bb71f539867a2a59f0d25ddfdf50399
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-07-12 19:33:55 +0000

    sign in and me add

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
 
@@ -19,3 +20,10 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+    # NOTE: In a real application make sure to properly hash and salt passwords
+    # Sources: http://www.programcreek.com/python/example/92986/werkzeug.security.safe_str_cmp
+    # http://stackoverflow.com/questions/59131067/what-is-the-purpose-of-safe-string-cmp
+    def check_password(self, password_param):
+        return safe_str_cmp(self.password.encode('utf-8'), password_param.encode("utf-8"))
